0754b975706a7579e0867c3e3b3be9ef4427bcbe
Het Makadia <hetmakadia.28@gmail.com>
2023-12-21 08:18:24 -0800

    Changes: Some Basic Changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,18 @@
 
 import React from "react";
+import logo from "../Navbar/SupremeCarbonLogo.png";
+import {Link} from "react-scroll";
 
 function Home() {
     return (
         <section id="home" className="text-primary bg-base-100">
-            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-                <div className="mx-auto max-w-lg text-center">
-                    <h2 className="text-3xl font-bold lg:text-5xl">Home</h2>
+            <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 grid grid-cols-12">
+                <div className="mx-auto max-w-lg text-center col-span-4 mt-8 self-center">
+                    <h2 className="text-3xl font-bold lg:text-5xl">About Us</h2>
                 </div>
-                <div className="mt-8">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eget egestas purus viverra accumsan in nisl. Dictum at tempor commodo ullamcorper a lacus vestibulum sed. Faucibus vitae aliquet nec ullamcorper sit amet risus. Sit amet facilisis magna etiam tempor orci eu. Mi sit amet mauris commodo quis. Condimentum id venenatis a condimentum. Egestas integer eget aliquet nibh praesent. Elit scelerisque mauris pellentesque pulvinar. Interdum consectetur libero id faucibus nisl. Augue interdum velit euismod in pellentesque massa. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Auctor neque vitae tempus quam. Ut tellus elementum sagittis vitae et leo duis ut. Semper eget duis at tellus at urna. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque sit. Viverra orci sagittis eu volutpat. Vitae ultricies leo integer malesuada nunc vel risus. Egestas pretium aenean pharetra magna ac. Tincidunt tortor aliquam nulla facilisi cras.
-
-                    Scelerisque viverra mauris in aliquam sem. Sed libero enim sed faucibus. Gravida dictum fusce ut placerat orci nulla pellentesque dignissim enim. Vulputate odio ut enim blandit volutpat. Arcu cursus euismod quis viverra nibh. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Nisl tincidunt eget nullam non nisi est sit amet facilisis. Eget egestas purus viverra accumsan in nisl. Nisl tincidunt eget nullam non nisi est. Nisi porta lorem mollis aliquam ut porttitor leo a diam. Facilisis volutpat est velit egestas. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Elementum integer enim neque volutpat ac tincidunt vitae semper. Ipsum suspendisse ultrices gravida dictum fusce ut. Sed euismod nisi porta lorem mollis.
+                <div className="mt-8 col-span-8 text-center">
+                    <li><a className="text-center normal-case text-2xl font-bold"><img src={logo} alt="Supreme Carbon" className="hover:animate-spin h-44"></img></a></li>
+                    Supreme Carbon is an industry-leading producer and provider of standard and customized activated carbons. Supreme Carbon is an prominent producer and provider of top-grade enacted carbon items from chosen grades of coconut, wood, and coal. We are known for our tremendous item run and extraordinary quality. Supreme Carbon has set up itself as a chief producer of activated carbons totally different grades and sizes such as powdered, granular and pelletized to title some
                 </div>
             </div>
         </section>
